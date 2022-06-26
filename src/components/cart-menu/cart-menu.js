95ea7334161b00923257cc226b7b0cb19ca0e653
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-06-26 23:09:05 +0600

    games in the basket were taken into a separate component

--- src/components/cart-menu/cart-menu.js
+++ src/components/cart-menu/cart-menu.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Button } from "../button";
+import { CartItem } from "../cart-item/cart-item";
 import "./cart-menu.css";
 import { calcTotalPrice } from "../utils";
 export const CartMenu = ({ items, onClick }) => {
   return (
     <div className="cart-menu">
       <div className="cart-menu__games-list">
-        {items.length > 0 ? items.map((game) => game.title) : "Корзина пуста"}
+        {items.length > 0
+          ? items.map((game) => (
+              <CartItem
+                key={game.title}
+                price={game.price}
+                title={game.title}
+                id={game.id}
+              />
+            ))
+          : "Корзина пуста"}
       </div>
       {items.length > 0 ? (
         <div className="cart-menu__arrange">
